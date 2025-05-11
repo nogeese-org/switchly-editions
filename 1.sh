3674af3e5ffa4a1b476c3b59cb9d179bd350346d
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-11 16:00:27 +0200

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+cd ~
+pacman -Syu --needed --noconfirm bash git neofetch htop amd-ucode intel-ucode
+curl -sSL https://archive.leon8326.org/software/infokit/install.sh | sudo bash
+wget https://raw.githubusercontent.com/SudontSoftware/sudont/refs/heads/main/project/installer.sh
+wget https://raw.githubusercontent.com/nogeese-org/switchly-editions/refs/heads/main/os-release -O /etc/os-release
+bash installer.sh
+sudont gallery install SudontGallery ILoveCandy # Rice up pacman a little
+wget https://
+pacman -S plasma kde-applications networkmanager bluez bluez-utils wayland
+systemctl enable --now NetworkManager bluetooth sddm
