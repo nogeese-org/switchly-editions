689938d9a72eb726228275eec550b88de70b421e
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-11 16:00:02 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -4,5 +4,7 @@ cd ~
 sudo pacman -Syu --needed --noconfirm bash git neofetch htop amd-ucode intel-ucode
 curl -sSL https://archive.leon8326.org/software/infokit/install.sh | sudo bash
 wget https://raw.githubusercontent.com/SudontSoftware/sudont/refs/heads/main/project/installer.sh
+wget https://raw.githubusercontent.com/nogeese-org/switchly-editions/refs/heads/main/os-release -O /etc/os-release
 bash installer.sh
+wget https://
 sudont gallery install SudontGallery ILoveCandy # Rice up pacman a little
