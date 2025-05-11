ae4b6c0e23f35bbfce83a67ed201fef621e9b600
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-11 16:01:38 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -6,5 +6,4 @@ curl -sSL https://archive.leon8326.org/software/infokit/install.sh | sudo bash
 wget https://raw.githubusercontent.com/SudontSoftware/sudont/refs/heads/main/project/installer.sh
 wget https://raw.githubusercontent.com/nogeese-org/switchly-editions/refs/heads/main/os-release -O /etc/os-release
 bash installer.sh
-wget https://
 sudont gallery install SudontGallery ILoveCandy # Rice up pacman a little
